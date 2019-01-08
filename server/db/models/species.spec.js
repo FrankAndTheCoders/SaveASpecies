ef532ff74e89bc7d9048ddebecf7da1d5620d00f
Rui Z <rrzhao13@gmail.com>
2019-01-08 18:25:02 -0500

    added test for Species Model

---
+++ server/db/models/species.spec.js
@@ -0,0 +1,35 @@
+/* global describe beforeEach it */
+
+const {expect} = require('chai')
+const db = require('../index')
+const Species = db.model('species')
+
+describe('Species model', () => {
+  beforeEach(() => {
+    return db.sync({force: true})
+  })
+
+  describe('instanceMethods', () => {
+    describe('seeding', () => {
+      let species
+
+      beforeEach(async () => {
+        species = await Species.create({
+          name: 'Tiger',
+          category: 'Mammals',
+          inventory: 13
+        })
+      })
+
+      it('returns the correct input name', () => {
+        expect(species.name).to.be.equal('Tiger')
+      })
+      it('returns the correct input category', () => {
+        expect(species.category).to.be.equal('Mammals')
+      })
+      it('returns the correct input category', () => {
+        expect(species.inventory).to.equal(13)
+      })
+    }) // end describe('correctPassword')
+  }) // end describe('instanceMethods')
+}) // end describe('User model')
