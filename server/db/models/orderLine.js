7815c54a3d85ad18ab165939b1841e870940e444
Frank Rose <franklin.rose@gmail.com>
2019-01-08 22:13:15 -0500

    test saving an object

--- server/db/models/orderLine.js
+++ server/db/models/orderLine.js
@@ -1,6 +1,17 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
 
-const OrderLine = db.define('orderLine', {})
+const OrderLine = db.define('orderLine', {
+  quantity: {
+    type: Sequelize.INTEGER
+    // allowNull: false,
+    // defaultValue: 1
+  },
+  subTotal: {
+    type: Sequelize.INTEGER,
+    allowNull: true,
+    defaultValue: 0
+  }
+})
 
 module.exports = OrderLine
--- server/db/models/tests/orderLine.spec.js
+++ server/db/models/tests/orderLine.spec.js
@@ -1,2 +1,24 @@
+/* global describe beforeEach it */
+
 const {expect} = require('chai')
-const db = require('../index')
+const db = require('../../index')
+// const {OrderLine} = require('../index')
+const OrderLine = db.model('orderLine')
+
+describe('OrderLine model', () => {
+  beforeEach(() => {
+    return db.sync({force: true})
+  })
+
+  describe('saving an object', async () => {
+    try {
+      let line = await OrderLine.create({quantity: 11, subTotal: 11200})
+      // console.log(line)
+    } catch (error) {
+      console.log(error)
+    }
+    it('returns the correct value for each field', () => {
+      expect(false).to.be.equal(true)
+    })
+  })
+})
