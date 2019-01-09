a7e16d90f2588d153318fb251c22f5e6d829862c
Frank Rose <franklin.rose@gmail.com>
2019-01-09 12:05:24 -0500

    Relations added

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,7 +2,7 @@ const User = require('./user')
 const Species = require('./species')
 const Price = require('./price')
 const OrderLine = require('./orderLine')
-// const Order = require('./order')
+const Order = require('./order')
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -10,6 +10,18 @@ const OrderLine = require('./orderLine')
  *
  *    BlogPost.belongsTo(User)
  */
+
+//  OrderLine Table
+Order.hasMany(OrderLine)
+Species.hasMany(OrderLine)
+Price.hasMany(OrderLine)
+OrderLine.belongsTo(Order)
+
+//  Order Table
+Order.belongsTo(User)
+
+//  Price Table
+Price.belongsTo(Species)
 
 /**
  * We'll export all of our models here, so that any time a module needs a model,
@@ -21,6 +33,6 @@ module.exports = {
   User,
   Species,
   Price,
-  OrderLine
-  // Order
+  OrderLine,
+  Order
 }
