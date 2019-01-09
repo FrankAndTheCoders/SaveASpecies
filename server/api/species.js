088e27a4db7946dcc711494ef13a89beb0f127c2
Stuart Jehn <stuartjehn@gmail.com>
2019-01-09 11:33:23 -0500

    speciesId route completed

--- server/api/species.js
+++ server/api/species.js
@@ -3,8 +3,17 @@ const {Species} = require('../db/models')
 
 router.get('/', async (req, res, next) => {
   try {
-    const species = await Species.findAll()
-    res.json(species)
+    const allSpecies = await Species.findAll()
+    res.json(allSpecies)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const speciesType = await Species.findByPk(req.params.id)
+    res.json(speciesType)
   } catch (err) {
     next(err)
   }
