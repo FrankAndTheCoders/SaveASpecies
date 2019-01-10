f9ef73c7cfbcd964209f4f7afd1a9454bde1bffe
Frank Rose <franklin.rose@gmail.com>
2019-01-10 15:09:35 -0500

    Updated route to include price

--- server/api/species.js
+++ server/api/species.js
@@ -1,15 +1,12 @@
 const router = require('express').Router()
 const {Species, Price} = require('../db/models')
+const db = require('../db')
 
 router.get('/', async (req, res, next) => {
   try {
-    const allSpecies = await Species.findAll({
-      include: [
-        {
-          model: Price
-        }
-      ]
-    })
+    const allSpecies = await db.query(`select DISTINCT ON (p."speciesId") p."currentPrice", s."id", s."name", s."category", s."inventory", s."description", s."ImageUrl"
+from prices p left join species s ON p."speciesId" = s."id"
+where p."effectiveDate" < now();`)
     res.json(allSpecies)
   } catch (err) {
     next(err)
