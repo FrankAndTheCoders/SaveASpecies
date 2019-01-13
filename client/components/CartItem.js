9d610b8313153d55fff0fd0b460942a94ef59aa0
Frank Rose <franklin.rose@gmail.com>
2019-01-12 20:58:40 -0500

    buttons on cart items
    Added FAB buttons to increase/decrease quantity

--- client/components/CartItem.js
+++ client/components/CartItem.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import Avatar from '@material-ui/core/Avatar'
+import Fab from '@material-ui/core/Fab'
+import Divider from '@material-ui/core/Divider'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemAvatar from '@material-ui/core/ListItemAvatar'
 import ListItemText from '@material-ui/core/ListItemText'
+import ToolTip from '@material-ui/core/Tooltip'
 
 const CartItem = props => {
   const {animal} = props
@@ -12,6 +15,19 @@ const CartItem = props => {
         <Avatar src="/panda-face-icon.png" />
       </ListItemAvatar>
       <ListItemText primary={animal} />
+      <Divider />
+      <div>
+        <ToolTip title="Less" aria-label="Less!">
+          <Fab color="inherit" size="small">
+            -
+          </Fab>
+        </ToolTip>
+        <ToolTip title="More" aria-label="More!">
+          <Fab color="inherit" size="small">
+            +
+          </Fab>
+        </ToolTip>
+      </div>
     </ListItem>
   )
 }
