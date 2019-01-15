308a5e4e0b4b5893e6641d34e136a9dc5bfd36c6
Frank Rose <franklin.rose@gmail.com>
2019-01-15 11:36:15 -0500

    Checkout button takes user to checkout page

--- client/components/PlaceOrder.js
+++ client/components/PlaceOrder.js
@@ -1,21 +1,37 @@
 import React from 'react'
+import {connect} from 'react-redux'
 import {Link, withRouter} from 'react-router-dom'
 
 import Fab from '@material-ui/core/Fab'
 import ShoppingBasketOutlined from '@material-ui/icons/ShoppingBasketOutlined'
 
 class PlaceOrder extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+  handleClick() {
+    this.props.history.push('/checkout')
+  }
   render() {
     const {cart} = this.props
+    console.log('Handle Click2:')
+    console.log(this.props)
     return (
-      <Link to="/checkout">
-        <Fab variant="extended" color="primary" disabled={cart.length === 0}>
-          <ShoppingBasketOutlined />
-          Checkout
-        </Fab>
-      </Link>
+      <Fab
+        variant="extended"
+        color="primary"
+        disabled={cart.length === 0}
+        onClick={() => this.handleClick()}
+      >
+        <ShoppingBasketOutlined />
+        Checkout
+      </Fab>
     )
   }
 }
 
-export default PlaceOrder
+// const mapStateToProps = state => ({})
+
+export default withRouter(PlaceOrder)
