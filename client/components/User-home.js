011ebd9028d64068e14818e4f44030b33b92f8a7
Rui Z <rrzhao13@gmail.com>
2019-01-15 23:45:36 -0500

    user page component complete

--- client/components/User-home.js
+++ client/components/User-home.js
@@ -2,6 +2,39 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {me} from '../store/user'
 import {getOrderHistory} from '../store/orderHistory'
+import PropTypes from 'prop-types'
+import {
+  withStyles,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Paper,
+  Grid,
+  Typography
+} from '@material-ui/core'
+
+const CustomTableCell = withStyles(theme => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white
+  },
+  body: {
+    fontSize: 14
+  }
+}))(TableCell)
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto'
+  },
+  table: {
+    minWidth: 700
+  }
+})
 
 class UserHome extends Component {
   componentDidMount() {
@@ -9,32 +42,65 @@ class UserHome extends Component {
     this.props.getOrderHistory(this.props.id)
   }
   render() {
-    console.log('ORDER HISTORY', this.props.orderHistory)
-    const {firstName, email, orderHistory} = this.props
+    const {firstName, email, orderHistory, classes} = this.props
     return (
-      <div>
-        <h2>Welcome {firstName}!</h2>
-        <h3>Email: {email}</h3>
-        <div>Order History:</div>
-        <div>
-          {orderHistory.map(order => (
-            <div>
-              <p>Order #: {order.id}</p>
-              {order.orderLines.map(orderLine => (
-                <div>
-                  <p>{orderLine.species.name}</p>
-                  <p>Quantity: {orderLine.quantity}</p>
-                  <p>SubTotal:{`$${orderLine.subTotal / 100}`}</p>
-                </div>
-              ))}
-              <p> Total Amount: {order.totalAmount} </p>
-              <p>Purchase Date: {order.purchaseDate.split('T')[0]}</p>
-            </div>
-          ))}
-        </div>
-      </div>
+      <Grid container alignItems="center" direction="column">
+        <Grid item>
+          <Typography variant="h4">Welcome {firstName}!</Typography>
+          <Typography variant="h5">Email: {email}</Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant="h6">Order History:</Typography>
+          <Paper className={classes.root}>
+            {orderHistory.map(order => (
+              <Table className={classes.table} key={order.id}>
+                <TableHead>
+                  <TableRow>
+                    <CustomTableCell align="right">
+                      Order # {order.id}
+                    </CustomTableCell>
+                    <CustomTableCell align="right">Species</CustomTableCell>
+                    <CustomTableCell align="right">Qty.</CustomTableCell>
+                    <CustomTableCell align="right">Subtotal</CustomTableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {order.orderLines.map(orderLine => (
+                    <TableRow key={orderLine.id}>
+                      <TableCell align="right" />
+                      <TableCell align="right">
+                        {orderLine.species.name}
+                      </TableCell>
+                      <TableCell align="right">{orderLine.quantity}</TableCell>
+                      <TableCell align="right">
+                        {`$${orderLine.subTotal / 100}`}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                  <TableRow>
+                    <TableCell rowSpan={3} />
+                    <TableCell colSpan={2}>Total</TableCell>
+                    <TableCell align="right">{`$${order.totalAmount /
+                      100}`}</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>Purchase Date</TableCell>
+                    <TableCell align="right">
+                      {order.purchaseDate.split('T')[0]}
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            ))}
+          </Paper>
+        </Grid>
+      </Grid>
     )
   }
+}
+
+UserHome.propTypes = {
+  classes: PropTypes.object.isRequired
 }
 
 const mapState = state => ({
@@ -49,4 +115,4 @@ const mapDispatch = dispatch => ({
   getOrderHistory: userId => dispatch(getOrderHistory(userId))
 })
 
-export default connect(mapState, mapDispatch)(UserHome)
+export default withStyles(styles)(connect(mapState, mapDispatch)(UserHome))
